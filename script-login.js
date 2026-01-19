b35d8368ccd2fd42903475c3373e32dee848d299
JoseRivera-07 <miguel.quiroz0310@gmail.com>
2026-01-18 20:05:31 -0500

    feat: Javascript añadido, guarda email en el localStorage y redirige

---
+++ script-login.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    const email = document.getElementById("email").value.trim();
+
+    if (email === "") {
+        alert("Por favor ingresa tu email o número");
+        return;
+    }
+
+    localStorage.setItem("userEmail", email);
+
+    window.location.replace("index.html");
+});
